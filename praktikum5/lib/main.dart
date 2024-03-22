233d5da012c83ae4c23168f88db22568b324fa09
ariapandu <ariapandu24@gmail.com>
2024-03-22 07:28:13 +0700

    jadi prak5

--- praktikum5/lib/main.dart
+++ praktikum5/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/second_page.dart';
 
 import 'home-page.dart';
 
@@ -11,9 +12,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/second_page': (context) => const SecondPage(),
+      },
     );
   }
 }
+
