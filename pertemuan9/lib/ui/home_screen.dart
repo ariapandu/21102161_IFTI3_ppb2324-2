18002bd57e3f5f5a28f6e927fcb5038a68014fd7
ariapandu <ariapandu24@gmail.com>
2024-05-30 22:49:21 +0700

    praktikum 12 (Buat Controller untuk inputan dari user.)

--- pertemuan9/lib/ui/home_screen.dart
+++ pertemuan9/lib/ui/home_screen.dart
@@ -3,41 +3,50 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pertemuan9/ui/login.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController noteController = TextEditingController();
+  @override
+  void dispose() {
+    titleController.dispose();
+    noteController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "HOME SCREEN",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff3D4DE0),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                GoogleSignIn().signOut();
-                FirebaseAuth.instance.signOut().then(
-                      (value) => Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginScreen(),
-                        ),
-                        (route) => false,
-                      ),
-                    );
-              },
-              child: const Text('Keluar'),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Note'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              GoogleSignIn().signOut();
+              FirebaseAuth.instance.signOut().then((value) {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginScreen(),
+                  ),
+                  (route) => false,
+                );
+              });
+            },
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
